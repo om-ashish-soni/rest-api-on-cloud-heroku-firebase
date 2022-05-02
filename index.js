9f35e07e75e6920019538b075e85614e4866dc46
Om Ashishkumar Soni <oms279400@gmail.com>
2022-05-02 11:52:50 +0530

    trying

--- index.js
+++ index.js
@@ -3,8 +3,23 @@ const cors=require('cors')
 const app=express()
 app.use(cors())
 const PORT =8093
+const courses=[
+    {
+        "name":"standard-10",
+        "fees":"5400",
+        "duration":"10 months"
+    },
+    {
+        "name":"standard-12",
+        "fees":"6600",
+        "duration":"12 months"
+    },
+]
 app.get('/',(req,res)=>{
     res.send("hello world")
+})
+app.get('/courses',(req,res)=>{
+    res.send(courses);
 })
 app.listen(process.env.PORT,()=>{
     console.log("shreepad shree vallabh is blessing you on port "+PORT)
