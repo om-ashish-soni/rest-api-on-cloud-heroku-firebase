5fe554c518aae0b016de2cfe7125163087e7d93c
Om Ashishkumar Soni <oms279400@gmail.com>
2022-05-05 18:03:32 +0530

    manipulating Date class

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,12 +1,11 @@
-const { Admin } = require('mongodb')
 const {ADMIN_KEY}=require('../credentials/credentails')
 const authMiddleware=(req,res,next)=>{
     const admin=req.body.admin
     if(admin && admin.key === ADMIN_KEY){
         req.admin=true
-        console.log("before calling next() ...")
+        console.log("advice before calling next() ...")
         next()
-        console.log("after calling next() ...")
+        console.log("advice after calling next() ...")
         return
     }else{
         res.status(403)
--- middleware/middleware.js
+++ middleware/middleware.js
@@ -1,7 +1,10 @@
 const {authMiddleware}=require('./authMiddleware')
 const middleware=(req,res,next)=>{
-    console.log(`${req.method} request to ${req.originalUrl}`)
+    const date=new Date()
+    console.log(`${req.method} request to ${req.originalUrl} on ${date.toLocaleDateString()} at ${date.toLocaleTimeString()}`)
+    console.log(`utc date & time : ${date.toUTCString()}`)
     next()
+    
 }
 exports.middleware=middleware
 exports.authMiddleware=authMiddleware
